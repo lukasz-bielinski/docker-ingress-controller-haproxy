23d4dcbdbef6af1fb53567af3bf1d2cdc2befa7d
pulse <antynomia@gmail.com>
2017-06-01 22:52:12 +0200

    remove maxconn

--- config/endpoints_script.sh
+++ config/endpoints_script.sh
@@ -26,7 +26,7 @@ do
 
  for port in $PORTS; do
    echo -e  "frontend front_"$svc"_$port\n    bind *:$port\n    mode tcp\n    default_backend backend_"$svc"_$port\n" >> /config/test.cfg
-   echo -e  "backend backend_"$svc"_$port\n    mode tcp\n    balance leastconn\n    maxconn 10000  " >> /config/test.cfg
+   echo -e  "backend backend_"$svc"_$port\n    mode tcp\n    balance leastconn  " >> /config/test.cfg
    for ip in $IPS; do
      echo -e  "    server $ip \t $ip:$port \t inter 1s fastinter 1s check " >> /config/test.cfg
    done
