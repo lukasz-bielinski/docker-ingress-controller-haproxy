4de7bb32e85e78f365f4ebcfdd5c03f151e8dae0
pulse <antynomia@gmail.com>
2017-06-25 02:15:21 +0200

    true seamless reload

--- config/notifyscript.sh
+++ config/notifyscript.sh
@@ -7,17 +7,19 @@ LOGPATH="/"
 case $STATE in
         "MASTER") touch $LOGPATH/keepalive.logs
                   echo "$NOW Becoming MASTER" >> $LOGPATH/keepalive.logs
+                  haproxy  -W -D -f /config/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid) -x /var/run/haproxy.sock >> $LOGPATH/keepalive.logs
                   exit 0
                   ;;
         "BACKUP") rm $KEEPALIVED/MASTER
                   echo "$NOW Becoming BACKUP" >> $LOGPATH/keepalive.logs
+                  haproxy  -W -D -f /config/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid) -x /var/run/haproxy.sock >> $LOGPATH/keepalive.logs
                   exit 0
                   ;;
         "FAULT")  rm $KEEPALIVED/MASTER
                   echo "$NOW Becoming FAULT" >> $LOGPATH/keepalive.logs
                   echo ""
                   echo "$NOW Trying to launch haproxy..." >> $LOGPATH/keepalive.logs
-                  pkill -9 haproxy;    haproxy  -W -D -f /config/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid) -x /var/run/haproxy.sock >> $LOGPATH/keepalive.logs
+                  haproxy  -W -D -f /config/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid) -x /var/run/haproxy.sock >> $LOGPATH/keepalive.logs
                   exit 0
                   ;;
         *)        echo "unknown state"
